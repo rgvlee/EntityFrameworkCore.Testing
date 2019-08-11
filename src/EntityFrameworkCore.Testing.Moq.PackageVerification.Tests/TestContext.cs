ea64a1ea635df44b3dbe4f2cbfc844ce2f156b81
Lee Anderson <arrgeeveelee@gmail.com>
2019-08-11 20:04:45 +0800

    Added package verification project Added readme tests

---
+++ src/EntityFrameworkCore.Testing.Moq.PackageVerification.Tests/TestContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Testing.Moq.PackageVerification.Tests {
+    public class TestContext : DbContext {
+
+        public virtual DbSet<TestEntity1> TestEntities { get; set; }
+        public virtual DbQuery<TestEntity2> TestView { get; set; }
+
+        public TestContext() {
+
+        }
+
+        public TestContext(DbContextOptions<TestContext> options) : base(options) {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Query<TestEntity2>().ToView("SomeView");
+        }
+    }
+}
---
+++ src/EntityFrameworkCore.Testing.Moq.PackageVerification.Tests/TestEntity2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace EntityFrameworkCore.Testing.Moq.PackageVerification.Tests {
+    [DebuggerDisplay("{nameof(Id)}: {Id}")]
+    public class TestEntity2 {
+        public Guid Id { get; set; }
+
+        public TestEntity2() {
+
+        }
+
+        public TestEntity2(Guid id) {
+            Id = id;
+        }
+    }
+}
