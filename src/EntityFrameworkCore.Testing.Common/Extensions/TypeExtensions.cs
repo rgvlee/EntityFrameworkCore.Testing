2b6720f05e1edced22aced2d35d8c05044530896
Lee Anderson <arrgeeveelee@gmail.com>
2019-10-27 14:08:35 +0800

    Cleaned up comments

--- src/EntityFrameworkCore.Testing.Common/Extensions/TypeExtensions.cs
+++ src/EntityFrameworkCore.Testing.Common/Extensions/TypeExtensions.cs
@@ -2,15 +2,11 @@
 
 namespace EntityFrameworkCore.Testing.Common.Extensions
 {
-    /// <summary>
-    ///     Extensions for the type type.
-    /// </summary>
+    /// <summary>Extensions for the <see cref="Type" /> type.</summary>
     public static class TypeExtensions
     {
-        /// <summary>
-        ///     Gets the default value for the specified type.
-        /// </summary>
-        /// <param name="type">The type instance.</param>
+        /// <summary>Gets the default value for the specified type.</summary>
+        /// <param name="type">The type to get the default value for.</param>
         /// <returns>The default value for the specified type.</returns>
         public static object GetDefaultValue(this Type type)
         {
--- src/EntityFrameworkCore.Testing.Common/Helpers/LoggerHelper.cs
+++ src/EntityFrameworkCore.Testing.Common/Helpers/LoggerHelper.cs
@@ -3,24 +3,18 @@ using Microsoft.Extensions.Logging;
 
 namespace EntityFrameworkCore.Testing.Common.Helpers
 {
-    /// <summary>
-    ///     A helper for creating loggers.
-    /// </summary>
+    /// <summary>A helper for creating loggers.</summary>
     public static class LoggerHelper
     {
         private static ILoggerFactory _factory;
 
-        /// <summary>
-        ///     Gets or sets the logger factory used to create loggers.
-        /// </summary>
+        /// <summary>Gets or sets the logger factory used to create loggers.</summary>
         public static ILoggerFactory LoggerFactory {
             get => _factory ?? (_factory = new LoggerFactory());
             set => _factory = value;
         }
 
-        /// <summary>
-        ///     Creates a new logger instance using the full name of the specified type.
-        /// </summary>
+        /// <summary>Creates a new logger instance using the full name of the specified type.</summary>
         /// <typeparam name="T">The type.</typeparam>
         /// <returns>A new logger instance.</returns>
         public static ILogger<T> CreateLogger<T>()
@@ -28,9 +22,7 @@ namespace EntityFrameworkCore.Testing.Common.Helpers
             return LoggerFactory.CreateLogger<T>();
         }
 
-        /// <summary>
-        ///     Creates a new logger instance.
-        /// </summary>
+        /// <summary>Creates a new logger instance.</summary>
         /// <param name="categoryName">The category name for messages produced by the logger.</param>
         /// <returns>A new logger instance.</returns>
         public static ILogger CreateLogger(string categoryName)
@@ -40,9 +32,7 @@ namespace EntityFrameworkCore.Testing.Common.Helpers
             return LoggerFactory.CreateLogger(categoryName);
         }
 
-        /// <summary>
-        ///     Creates a new logger instance using the full name of the specified type.
-        /// </summary>
+        /// <summary>Creates a new logger instance using the full name of the specified type.</summary>
         /// <param name="type">The type.</param>
         /// <returns>A new logger instance.</returns>
         public static ILogger CreateLogger(Type type)
