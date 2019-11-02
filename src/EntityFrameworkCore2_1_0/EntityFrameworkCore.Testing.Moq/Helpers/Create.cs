47764fe8c4c5b7b21188e293de26f036b4021ead
Lee Anderson <arrgeeveelee@gmail.com>
2019-11-02 14:31:14 +0800

    Reverted whitespace removal

--- src/EntityFrameworkCore2_1_0/EntityFrameworkCore.Testing.Moq/Helpers/Create.cs
+++ src/EntityFrameworkCore2_1_0/EntityFrameworkCore.Testing.Moq/Helpers/Create.cs
@@ -11,7 +11,8 @@ namespace EntityFrameworkCore.Testing.Moq.Helpers
         /// <typeparam name="TDbContext">The db context type.</typeparam>
         /// <param name="dbContextToMock">The db context to mock.</param>
         /// <returns>A mocked db context.</returns>
-        public static TDbContext MockedDbContextFor<TDbContext>(TDbContext dbContextToMock) where TDbContext : DbContext
+        public static TDbContext MockedDbContextFor<TDbContext>(TDbContext dbContextToMock)
+            where TDbContext : DbContext
         {
             EnsureArgument.IsNotNull(dbContextToMock, nameof(dbContextToMock));
 
@@ -22,7 +23,8 @@ namespace EntityFrameworkCore.Testing.Moq.Helpers
         /// <typeparam name="TEntity">The db set entity type.</typeparam>
         /// <param name="dbSetToMock">The db set to mock.</param>
         /// <returns>A mocked db set.</returns>
-        public static DbSet<TEntity> MockedDbSetFor<TEntity>(DbSet<TEntity> dbSetToMock) where TEntity : class
+        public static DbSet<TEntity> MockedDbSetFor<TEntity>(DbSet<TEntity> dbSetToMock)
+            where TEntity : class
         {
             EnsureArgument.IsNotNull(dbSetToMock, nameof(dbSetToMock));
 
@@ -33,7 +35,8 @@ namespace EntityFrameworkCore.Testing.Moq.Helpers
         /// <typeparam name="TQuery">The db query query type.</typeparam>
         /// <param name="dbQueryToMock">The db query to mock.</param>
         /// <returns>A mocked db query.</returns>
-        public static DbQuery<TQuery> MockedDbQueryFor<TQuery>(DbQuery<TQuery> dbQueryToMock) where TQuery : class
+        public static DbQuery<TQuery> MockedDbQueryFor<TQuery>(DbQuery<TQuery> dbQueryToMock)
+            where TQuery : class
         {
             EnsureArgument.IsNotNull(dbQueryToMock, nameof(dbQueryToMock));
 
