7154957771584c895ecdbb34a36b121e237683cd
Lee Anderson <arrgeeveelee@gmail.com>
2019-11-02 21:00:37 +0800

    Removed DbContext dependency, it's not needed Explicit set for fixed date time as part of instance creation Fixed method names Removed instances of new Fixture() and resorted to base test property Removed references to Queryable; using DbQuery instead R# code clean up

--- src/EntityFrameworkCore2_1_0/EntityFrameworkCore.Testing.Common.Tests/DbQueryTestsBase.cs
+++ src/EntityFrameworkCore2_1_0/EntityFrameworkCore.Testing.Common.Tests/DbQueryTestsBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using AutoFixture;
@@ -9,98 +10,79 @@ using NUnit.Framework;
 namespace EntityFrameworkCore.Testing.Common.Tests
 {
     [TestFixture]
-    public abstract class DbQueryTestsBase<TDbContext, TQuery> : MockQueryableTestsBase<TQuery>
-        where TDbContext : DbContext
+    public abstract class DbQueryTestsBase<TQuery> : MockQueryableTestsBase<TQuery>
         where TQuery : TestEntityBase
     {
         [SetUp]
         public override void SetUp()
         {
-            MockedDbContext = CreateMockedDbContext();
             base.SetUp();
         }
 
         protected override void SeedQueryableSource()
         {
-            var itemsToAdd = Fixture.CreateMany<TQuery>().ToList();
-            AddRange(DbQuery, itemsToAdd);
+            var itemsToAdd = Fixture.Build<TQuery>()
+                .With(p => p.FixedDateTime, DateTime.Parse("2019-01-01"))
+                .CreateMany().ToList();
+            AddRangeToReadOnlySource(DbQuery, itemsToAdd);
             //MockedDbContext.SaveChanges();
             ItemsAddedToQueryableSource = itemsToAdd;
         }
 
-        protected TDbContext MockedDbContext;
         protected DbQuery<TQuery> DbQuery => (DbQuery<TQuery>) Queryable;
 
-        protected abstract TDbContext CreateMockedDbContext();
-
-        protected abstract void Add(DbQuery<TQuery> mockedDbQuery, TQuery item);
-        protected abstract void AddRange(DbQuery<TQuery> mockedDbQuery, IEnumerable<TQuery> sequence);
-        protected abstract void Clear(DbQuery<TQuery> mockedDbQuery);
+        protected abstract void AddToReadOnlySource(DbQuery<TQuery> mockedDbQuery, TQuery item);
+        protected abstract void AddRangeToReadOnlySource(DbQuery<TQuery> mockedDbQuery, IEnumerable<TQuery> items);
+        protected abstract void ClearReadOnlySource(DbQuery<TQuery> mockedDbQuery);
 
         [Test]
-        public virtual void Add_Item_AddsItem()
+        public virtual void AddRangeToReadOnlySource_Items_AddsItemsToReadOnlySource()
         {
-            var expectedResult = new Fixture().Create<TQuery>();
+            var expectedResult = Fixture.CreateMany<TQuery>().ToList();
 
-            Add(DbQuery, expectedResult);
+            AddRangeToReadOnlySource(DbQuery, expectedResult);
+
+            Assert.That(DbQuery, Is.EquivalentTo(expectedResult));
+        }
+
+        [Test]
+        public virtual void AddRangeToReadOnlySourceThenAddRangeToReadOnlySource_Items_AddsAllItemsToReadOnlySource()
+        {
+            var expectedResult = Fixture.CreateMany<TQuery>(4).ToList();
+
+            AddRangeToReadOnlySource(DbQuery, expectedResult.Take(2));
+            AddRangeToReadOnlySource(DbQuery, expectedResult.Skip(2));
+
+            Assert.That(DbQuery, Is.EquivalentTo(expectedResult));
+        }
+
+        [Test]
+        public virtual void AddToReadOnlySource_Item_AddsItemToReadOnlySource()
+        {
+            var expectedResult = Fixture.Create<TQuery>();
+
+            AddToReadOnlySource(DbQuery, expectedResult);
             var numberOfItemsAdded = DbQuery.ToList().Count;
 
             Assert.That(numberOfItemsAdded, Is.EqualTo(1));
         }
 
         [Test]
-        public virtual void AddRange_Items_AddsItems()
+        public virtual void AddToReadOnlySourceThenAddToReadOnlySource_Items_AddsBothItemsToReadOnlySource()
         {
-            var expectedResult = new Fixture().CreateMany<TQuery>().ToList();
+            var expectedResult = Fixture.CreateMany<TQuery>(2).ToList();
 
-            AddRange(DbQuery, expectedResult);
+            AddToReadOnlySource(DbQuery, expectedResult.First());
+            AddToReadOnlySource(DbQuery, expectedResult.Last());
 
             Assert.That(DbQuery, Is.EquivalentTo(expectedResult));
         }
 
         [Test]
-        public virtual void AddRangeThenAddRange_Items_AddsAllItems()
-        {
-            var expectedResult = new Fixture().CreateMany<TQuery>(4).ToList();
-
-            AddRange(DbQuery, expectedResult.Take(2));
-            AddRange(DbQuery, expectedResult.Skip(2));
-
-            Assert.That(DbQuery, Is.EquivalentTo(expectedResult));
-        }
-
-        [Test]
-        public virtual void AddRangeThenClear_RemovesAllItems()
-        {
-            var expectedResult = new Fixture().CreateMany<TQuery>().ToList();
-            AddRange(DbQuery, expectedResult);
-            var numberOfItemsAdded = DbQuery.ToList().Count;
-
-            Clear(DbQuery);
-
-            Assert.Multiple(() =>
-            {
-                Assert.That(numberOfItemsAdded, Is.EqualTo(expectedResult.Count));
-                Assert.That(DbQuery.Any(), Is.False);
-            });
-        }
-
-        [Test]
-        public virtual void AddThenAdd_Items_AddsBothItems()
-        {
-            var expectedResult = new Fixture().CreateMany<TQuery>(2).ToList();
-
-            Add(DbQuery, expectedResult.First());
-            Add(DbQuery, expectedResult.Last());
-
-            Assert.That(DbQuery, Is.EquivalentTo(expectedResult));
-        }
-
-        [Test]
-        public virtual void AnyThenAddAndPersistThenAny_ReturnsFalseThenTrue()
+        public virtual void AnyThenAddToReadOnlySourceThenAny_ReturnsFalseThenTrue()
         {
             var actualResult1 = DbQuery.Any();
-            Add(DbQuery, Fixture.Create<TQuery>());
+            AddToReadOnlySource(DbQuery, Fixture.Create<TQuery>());
             var actualResult2 = DbQuery.Any();
 
             Assert.Multiple(() =>
@@ -111,11 +93,11 @@ namespace EntityFrameworkCore.Testing.Common.Tests
         }
 
         [Test]
-        public virtual void Clear_WithNoItemsAdded_DoesNothing()
+        public virtual void ClearReadOnlySource_WithNoItemsAddedToReadOnlySource_DoesNothing()
         {
             var preActNumberOfItems = DbQuery.ToList().Count;
 
-            Clear(DbQuery);
+            ClearReadOnlySource(DbQuery);
 
             var postActNumberOfItems = DbQuery.ToList().Count;
             Assert.Multiple(() =>
@@ -126,36 +108,52 @@ namespace EntityFrameworkCore.Testing.Common.Tests
         }
 
         [Test]
+        public virtual void ClearReadOnlySourceWithExistingItems_RemovesAllItemsFromReadOnlySource()
+        {
+            var expectedResult = Fixture.CreateMany<TQuery>().ToList();
+            AddRangeToReadOnlySource(DbQuery, expectedResult);
+            var numberOfItemsAdded = DbQuery.ToList().Count;
+
+            ClearReadOnlySource(DbQuery);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(numberOfItemsAdded, Is.EqualTo(expectedResult.Count));
+                Assert.That(DbQuery.Any(), Is.False);
+            });
+        }
+
+        [Test]
         public override void FromSql_QueryProviderWithManyFromSqlResults_ReturnsExpectedResults()
         {
             var sql1 = "sp_NoParams";
-            var expectedResult1 = new Fixture().CreateMany<TQuery>().ToList();
+            var expectedResult1 = Fixture.CreateMany<TQuery>().ToList();
 
             var sql2 = "sp_WithParams";
             var parameters2 = new List<SqlParameter> {new SqlParameter("@SomeParameter1", "Value1"), new SqlParameter("@SomeParameter2", "Value2")};
-            var expectedResult2 = new Fixture().CreateMany<TQuery>().ToList();
+            var expectedResult2 = Fixture.CreateMany<TQuery>().ToList();
 
-            AddFromSqlResult(Queryable, sql1, expectedResult1);
+            AddFromSqlResult(DbQuery, sql1, expectedResult1);
 
             //Change the source, this will force the query provider mock to aggregate
-            AddRange(DbQuery, new Fixture().CreateMany<TQuery>().ToList());
+            AddRangeToReadOnlySource(DbQuery, Fixture.CreateMany<TQuery>().ToList());
 
-            AddFromSqlResult(Queryable, sql2, parameters2, expectedResult2);
+            AddFromSqlResult(DbQuery, sql2, parameters2, expectedResult2);
 
             Logger.LogDebug("actualResult1");
-            var actualResult1 = Queryable.FromSql("[dbo].[sp_NoParams]").ToList();
+            var actualResult1 = DbQuery.FromSql("[dbo].[sp_NoParams]").ToList();
             Logger.LogDebug("actualResult2");
-            var actualResult2 = Queryable.FromSql("sp_NoParams").ToList();
+            var actualResult2 = DbQuery.FromSql("sp_NoParams").ToList();
 
             Logger.LogDebug("actualResult3");
-            var actualResult3 = Queryable.FromSql("[dbo].[sp_WithParams]").ToList();
+            var actualResult3 = DbQuery.FromSql("[dbo].[sp_WithParams]").ToList();
             Logger.LogDebug("actualResult4");
-            var actualResult4 = Queryable.FromSql("sp_WithParams @SomeParameter1 @SomeParameter2").ToList();
+            var actualResult4 = DbQuery.FromSql("sp_WithParams @SomeParameter1 @SomeParameter2").ToList();
 
             Logger.LogDebug("actualResult5");
-            var actualResult5 = Queryable.FromSql("[dbo].[sp_WithParams]", new List<SqlParameter> {new SqlParameter("@someparameter2", "value2")}).ToList();
+            var actualResult5 = DbQuery.FromSql("[dbo].[sp_WithParams]", new List<SqlParameter> {new SqlParameter("@someparameter2", "value2")}).ToList();
             Logger.LogDebug("actualResult6");
-            var actualResult6 = Queryable.FromSql("sp_WithParams @SomeParameter1 @SomeParameter2", new List<SqlParameter> {new SqlParameter("@someparameter2", "value2")}).ToList();
+            var actualResult6 = DbQuery.FromSql("sp_WithParams @SomeParameter1 @SomeParameter2", new List<SqlParameter> {new SqlParameter("@someparameter2", "value2")}).ToList();
 
             Assert.Multiple(() =>
             {
