5e50d8701ae89a8e4bfcac7c86d57053b37745eb
Lee Anderson <arrgeeveelee@gmail.com>
2019-11-09 14:23:53 +0800

    Added default behaviour tests to verify the default exception for executesql* methods

---
+++ src/EntityFrameworkCore2_1_0/EntityFrameworkCore.DefaultBehaviour.Tests/DbContextTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using EntityFrameworkCore.Testing.Common.Tests;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace EntityFrameworkCore.DefaultBehaviour.Tests
+{
+    [TestFixture]
+    public class DbContextTests : TestBase
+    {
+        [SetUp]
+        public override void SetUp()
+        {
+            DbContext = new TestDbContext(new DbContextOptionsBuilder<TestDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options);
+            base.SetUp();
+        }
+
+        protected TestDbContext DbContext;
+
+        [Test]
+        public virtual void ExecuteSqlCommand_ThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var actualResult = DbContext.Database.ExecuteSqlCommand("sp_NoParams");
+            });
+        }
+    }
+}
---
+++ src/EntityFrameworkCore3_0_0/EntityFrameworkCore.DefaultBehaviour.Tests/DbContextTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using EntityFrameworkCore.Testing.Common.Tests;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace EntityFrameworkCore.DefaultBehaviour.Tests
+{
+    [TestFixture]
+    public class DbContextTests : TestBase
+    {
+        [SetUp]
+        public override void SetUp()
+        {
+            DbContext = new TestDbContext(new DbContextOptionsBuilder<TestDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options);
+            base.SetUp();
+        }
+
+        protected TestDbContext DbContext;
+
+        [Test]
+        public virtual void ExecuteSqlCommand_ThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var actualResult = DbContext.Database.ExecuteSqlCommand("sp_NoParams");
+            });
+        }
+
+        [Test]
+        public virtual void ExecuteSqlInterpolated_ThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var actualResult = DbContext.Database.ExecuteSqlInterpolated($"sp_NoParams");
+            });
+        }
+
+        [Test]
+        public virtual void ExecuteSqlRaw_ThrowsException()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var actualResult = DbContext.Database.ExecuteSqlRaw("sp_NoParams");
+            });
+        }
+    }
+}
