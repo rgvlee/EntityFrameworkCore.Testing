66469ee841ea1fdb7f01d464c3e8ee8ddf1699ff
Lee Anderson <arrgeeveelee@gmail.com>
2019-11-09 14:24:33 +0800

    Added to array to allow parmeters to be accepted as params rather than a single list

--- src/EntityFrameworkCore2_1_0/EntityFrameworkCore.Testing.Common.Tests/DbQueryTestsBase.cs
+++ src/EntityFrameworkCore2_1_0/EntityFrameworkCore.Testing.Common.Tests/DbQueryTestsBase.cs
@@ -151,9 +151,9 @@ namespace EntityFrameworkCore.Testing.Common.Tests
             var actualResult4 = DbQuery.FromSql("sp_WithParams @SomeParameter1 @SomeParameter2").ToList();
 
             Logger.LogDebug("actualResult5");
-            var actualResult5 = DbQuery.FromSql("[dbo].[sp_WithParams]", new List<SqlParameter> {new SqlParameter("@someparameter2", "value2")}).ToList();
+            var actualResult5 = DbQuery.FromSql("[dbo].[sp_WithParams]", new List<SqlParameter> {new SqlParameter("@someparameter2", "value2")}.ToArray()).ToList();
             Logger.LogDebug("actualResult6");
-            var actualResult6 = DbQuery.FromSql("sp_WithParams @SomeParameter1 @SomeParameter2", new List<SqlParameter> {new SqlParameter("@someparameter2", "value2")}).ToList();
+            var actualResult6 = DbQuery.FromSql("sp_WithParams @SomeParameter1 @SomeParameter2", new List<SqlParameter> {new SqlParameter("@someparameter2", "value2")}.ToArray()).ToList();
 
             Assert.Multiple(() =>
             {
