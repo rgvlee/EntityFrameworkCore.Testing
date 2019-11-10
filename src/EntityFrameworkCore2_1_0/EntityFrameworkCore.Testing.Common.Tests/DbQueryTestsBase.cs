cf6c08e73ffc4f36b1d359d7c8e213deb256c515
Lee Anderson <arrgeeveelee@gmail.com>
2019-11-10 16:31:24 +0800

    Updated the multiple query provider test; previous test had flaws

--- src/EntityFrameworkCore2_1_0/EntityFrameworkCore.Testing.Common.Tests/DbQueryTestsBase.cs
+++ src/EntityFrameworkCore2_1_0/EntityFrameworkCore.Testing.Common.Tests/DbQueryTestsBase.cs
@@ -142,29 +142,14 @@ namespace EntityFrameworkCore.Testing.Common.Tests
 
             Logger.LogDebug("actualResult1");
             var actualResult1 = DbQuery.FromSql("[dbo].[sp_NoParams]").ToList();
+
             Logger.LogDebug("actualResult2");
-            var actualResult2 = DbQuery.FromSql("sp_NoParams").ToList();
-
-            Logger.LogDebug("actualResult3");
-            var actualResult3 = DbQuery.FromSql("[dbo].[sp_WithParams]").ToList();
-            Logger.LogDebug("actualResult4");
-            var actualResult4 = DbQuery.FromSql("sp_WithParams @SomeParameter1 @SomeParameter2").ToList();
-
-            Logger.LogDebug("actualResult5");
-            var actualResult5 = DbQuery.FromSql("[dbo].[sp_WithParams]", new List<SqlParameter> {new SqlParameter("@someparameter2", "value2")}.ToArray()).ToList();
-            Logger.LogDebug("actualResult6");
-            var actualResult6 = DbQuery.FromSql("sp_WithParams @SomeParameter1 @SomeParameter2", new List<SqlParameter> {new SqlParameter("@someparameter2", "value2")}.ToArray()).ToList();
+            var actualResult2 = DbQuery.FromSql("[dbo].[sp_WithParams]", parameters2.ToArray()).ToList();
 
             Assert.Multiple(() =>
             {
                 Assert.That(actualResult1, Is.EquivalentTo(expectedResult1));
-                Assert.That(actualResult2, Is.EquivalentTo(actualResult1));
-
-                Assert.That(actualResult3, Is.EquivalentTo(expectedResult2));
-                Assert.That(actualResult4, Is.EquivalentTo(actualResult3));
-
-                Assert.That(actualResult5, Is.EquivalentTo(expectedResult2));
-                Assert.That(actualResult6, Is.EquivalentTo(actualResult5));
+                Assert.That(actualResult2, Is.EquivalentTo(expectedResult2));
             });
         }
     }
--- src/EntityFrameworkCore2_1_0/EntityFrameworkCore.Testing.Common.Tests/MockQueryableTestsBase.cs
+++ src/EntityFrameworkCore2_1_0/EntityFrameworkCore.Testing.Common.Tests/MockQueryableTestsBase.cs
@@ -54,29 +54,14 @@ namespace EntityFrameworkCore.Testing.Common.Tests
 
             Logger.LogDebug("actualResult1");
             var actualResult1 = Queryable.FromSql("[dbo].[sp_NoParams]").ToList();
+
             Logger.LogDebug("actualResult2");
-            var actualResult2 = Queryable.FromSql("sp_NoParams").ToList();
-
-            Logger.LogDebug("actualResult3");
-            var actualResult3 = Queryable.FromSql("[dbo].[sp_WithParams]").ToList();
-            Logger.LogDebug("actualResult4");
-            var actualResult4 = Queryable.FromSql("sp_WithParams @SomeParameter1 @SomeParameter2").ToList();
-
-            Logger.LogDebug("actualResult5");
-            var actualResult5 = Queryable.FromSql("[dbo].[sp_WithParams]", new List<SqlParameter> {new SqlParameter("@someparameter2", "value2")}.ToArray()).ToList();
-            Logger.LogDebug("actualResult6");
-            var actualResult6 = Queryable.FromSql("sp_WithParams @SomeParameter1 @SomeParameter2", new List<SqlParameter> {new SqlParameter("@someparameter2", "value2")}.ToArray()).ToList();
+            var actualResult2 = Queryable.FromSql("[dbo].[sp_WithParams]", parameters2.ToArray()).ToList();
 
             Assert.Multiple(() =>
             {
                 Assert.That(actualResult1, Is.EquivalentTo(expectedResult1));
-                Assert.That(actualResult2, Is.EquivalentTo(actualResult1));
-
-                Assert.That(actualResult3, Is.EquivalentTo(expectedResult2));
-                Assert.That(actualResult4, Is.EquivalentTo(actualResult3));
-
-                Assert.That(actualResult5, Is.EquivalentTo(expectedResult2));
-                Assert.That(actualResult6, Is.EquivalentTo(actualResult5));
+                Assert.That(actualResult2, Is.EquivalentTo(expectedResult2));
             });
         }
 
