b7a0cebf9eb46eb140890022cd36c9a20f042174
Lee Anderson <arrgeeveelee@gmail.com>
2019-11-14 22:30:16 +0800

    Updated async enumerable to set an async query provider

--- src/EntityFrameworkCore 2.1.0-2.2.6/EntityFrameworkCore.Testing.Common/AsyncEnumerable.cs
+++ src/EntityFrameworkCore 2.1.0-2.2.6/EntityFrameworkCore.Testing.Common/AsyncEnumerable.cs
@@ -7,20 +7,33 @@ namespace EntityFrameworkCore.Testing.Common
 {
     /// <inheritdoc cref="IAsyncEnumerable{T}" />
     [ExcludeFromCodeCoverage]
-    public class AsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>
+    public class AsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable, IQueryable<T>
     {
+        private readonly IQueryProvider _provider;
+
         /// <summary>Constructor.</summary>
         /// <param name="expression">The expression to create an asynchronous enumerable for.</param>
-        public AsyncEnumerable(Expression expression) : base(expression) { }
+        public AsyncEnumerable(Expression expression) : base(expression)
+        {
+            var queryable = (IQueryable<T>) ((ConstantExpression) ((MethodCallExpression) expression).Arguments[0]).Value;
+            _provider = queryable.Provider;
+        }
 
         /// <summary>Constructor.</summary>
         /// <param name="enumerable">The enumerable to create an asynchronous enumerable for.</param>
-        public AsyncEnumerable(IEnumerable<T> enumerable) : base(enumerable) { }
+        public AsyncEnumerable(IEnumerable<T> enumerable) : base(enumerable)
+        {
+            var provider = new AsyncQueryProvider<T>();
+            provider.Source = enumerable.AsQueryable();
+            _provider = provider;
+        }
 
         /// <inheritdoc />
         public IAsyncEnumerator<T> GetEnumerator()
         {
             return new AsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
         }
+
+        IQueryProvider IQueryable.Provider => _provider;
     }
 }
