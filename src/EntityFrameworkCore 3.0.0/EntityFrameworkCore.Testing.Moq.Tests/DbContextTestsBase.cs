f7dd64b9f433d126ec7439927aa419bdd1b22916
Lee Anderson <arrgeeveelee@gmail.com>
2019-12-20 11:32:23 +0800

    Added a base for the db context tests to remove duplicate code

---
+++ src/EntityFrameworkCore 3.0.0/EntityFrameworkCore.Testing.Moq.Tests/DbContextTestsBase.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using EntityFrameworkCore.Testing.Moq.Extensions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace EntityFrameworkCore.Testing.Moq.Tests
+{
+    [TestFixture]
+    public class DbContextTestsBase<T> : Common.Tests.DbContextTestsBase<T> where T : DbContext
+    {
+        public override void AddExecuteSqlCommandResult(T mockedDbContext, int expectedResult)
+        {
+            mockedDbContext.AddExecuteSqlCommandResult(expectedResult);
+        }
+
+        public override void AddExecuteSqlCommandResult(T mockedDbContext, int expectedResult, Action callback)
+        {
+            mockedDbContext.AddExecuteSqlCommandResult(expectedResult, callback);
+        }
+
+        public override void AddExecuteSqlCommandResult(T mockedDbContext, string sql, int expectedResult)
+        {
+            mockedDbContext.AddExecuteSqlCommandResult(sql, expectedResult);
+        }
+
+        public override void AddExecuteSqlCommandResult(T mockedDbContext, string sql, int expectedResult, Action callback)
+        {
+            mockedDbContext.AddExecuteSqlCommandResult(sql, expectedResult, callback);
+        }
+
+        public override void AddExecuteSqlCommandResult(T mockedDbContext, string sql, IEnumerable<object> parameters, int expectedResult)
+        {
+            mockedDbContext.AddExecuteSqlCommandResult(sql, parameters, expectedResult);
+        }
+
+        public override void AddExecuteSqlCommandResult(T mockedDbContext, string sql, IEnumerable<object> parameters, int expectedResult, Action callback)
+        {
+            mockedDbContext.AddExecuteSqlCommandResult(sql, parameters, expectedResult, callback);
+        }
+
+        public override void AddExecuteSqlInterpolatedResult(T mockedDbContext, int expectedResult)
+        {
+            mockedDbContext.AddExecuteSqlInterpolatedResult(expectedResult);
+        }
+
+        public override void AddExecuteSqlInterpolatedResult(T mockedDbContext, FormattableString sql, int expectedResult)
+        {
+            mockedDbContext.AddExecuteSqlInterpolatedResult(sql, expectedResult);
+        }
+
+        public override void AddExecuteSqlInterpolatedResult(T mockedDbContext, string sql, IEnumerable<object> parameters, int expectedResult)
+        {
+            mockedDbContext.AddExecuteSqlInterpolatedResult(sql, parameters, expectedResult);
+        }
+
+        public override void AddExecuteSqlRawResult(T mockedDbContext, int expectedResult)
+        {
+            mockedDbContext.AddExecuteSqlRawResult(expectedResult);
+        }
+
+        public override void AddExecuteSqlRawResult(T mockedDbContext, string sql, int expectedResult)
+        {
+            mockedDbContext.AddExecuteSqlRawResult(sql, expectedResult);
+        }
+
+        public override void AddExecuteSqlRawResult(T mockedDbContext, string sql, IEnumerable<object> parameters, int expectedResult)
+        {
+            mockedDbContext.AddExecuteSqlRawResult(sql, parameters, expectedResult);
+        }
+    }
+}
--- src/EntityFrameworkCore 3.0.0/EntityFrameworkCore.Testing.Moq.Tests/DbContextTestsUsingType.cs
+++ src/EntityFrameworkCore 3.0.0/EntityFrameworkCore.Testing.Moq.Tests/DbContextTestsUsingType.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using EntityFrameworkCore.Testing.Common.Tests;
-using EntityFrameworkCore.Testing.Moq.Extensions;
+﻿using EntityFrameworkCore.Testing.Common.Tests;
 using EntityFrameworkCore.Testing.Moq.Helpers;
 using NUnit.Framework;
 
@@ -15,66 +12,6 @@ namespace EntityFrameworkCore.Testing.Moq.Tests
         {
             MockedDbContext = Create.MockedDbContextFor<TestDbContext>();
             base.SetUp();
-        }
-
-        public override void AddExecuteSqlCommandResult(TestDbContext mockedDbContext, int expectedResult)
-        {
-            mockedDbContext.AddExecuteSqlCommandResult(expectedResult);
-        }
-
-        public override void AddExecuteSqlCommandResult(TestDbContext mockedDbContext, int expectedResult, Action callback)
-        {
-            mockedDbContext.AddExecuteSqlCommandResult(expectedResult, callback);
-        }
-
-        public override void AddExecuteSqlCommandResult(TestDbContext mockedDbContext, string sql, int expectedResult)
-        {
-            mockedDbContext.AddExecuteSqlCommandResult(sql, expectedResult);
-        }
-
-        public override void AddExecuteSqlCommandResult(TestDbContext mockedDbContext, string sql, int expectedResult, Action callback)
-        {
-            mockedDbContext.AddExecuteSqlCommandResult(sql, expectedResult, callback);
-        }
-
-        public override void AddExecuteSqlCommandResult(TestDbContext mockedDbContext, string sql, IEnumerable<object> parameters, int expectedResult)
-        {
-            mockedDbContext.AddExecuteSqlCommandResult(sql, parameters, expectedResult);
-        }
-
-        public override void AddExecuteSqlCommandResult(TestDbContext mockedDbContext, string sql, IEnumerable<object> parameters, int expectedResult, Action callback)
-        {
-            mockedDbContext.AddExecuteSqlCommandResult(sql, parameters, expectedResult, callback);
-        }
-
-        public override void AddExecuteSqlInterpolatedResult(TestDbContext mockedDbContext, int expectedResult)
-        {
-            mockedDbContext.AddExecuteSqlInterpolatedResult(expectedResult);
-        }
-
-        public override void AddExecuteSqlInterpolatedResult(TestDbContext mockedDbContext, FormattableString sql, int expectedResult)
-        {
-            mockedDbContext.AddExecuteSqlInterpolatedResult(sql, expectedResult);
-        }
-
-        public override void AddExecuteSqlInterpolatedResult(TestDbContext mockedDbContext, string sql, IEnumerable<object> parameters, int expectedResult)
-        {
-            mockedDbContext.AddExecuteSqlInterpolatedResult(sql, parameters, expectedResult);
-        }
-
-        public override void AddExecuteSqlRawResult(TestDbContext mockedDbContext, int expectedResult)
-        {
-            mockedDbContext.AddExecuteSqlRawResult(expectedResult);
-        }
-
-        public override void AddExecuteSqlRawResult(TestDbContext mockedDbContext, string sql, int expectedResult)
-        {
-            mockedDbContext.AddExecuteSqlRawResult(sql, expectedResult);
-        }
-
-        public override void AddExecuteSqlRawResult(TestDbContext mockedDbContext, string sql, IEnumerable<object> parameters, int expectedResult)
-        {
-            mockedDbContext.AddExecuteSqlRawResult(sql, parameters, expectedResult);
         }
     }
 }
