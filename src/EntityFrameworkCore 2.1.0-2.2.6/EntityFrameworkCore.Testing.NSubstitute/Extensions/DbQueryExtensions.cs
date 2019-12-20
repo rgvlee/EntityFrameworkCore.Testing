64d1f9aabeb892fb641f114b2d8fffd66ed0b83d
Lee Anderson <arrgeeveelee@gmail.com>
2019-12-20 19:07:05 +0800

    Replaced mocked with substitute

--- src/EntityFrameworkCore 2.1.0-2.2.6/EntityFrameworkCore.Testing.NSubstitute/Extensions/DbQueryExtensions.cs
+++ src/EntityFrameworkCore 2.1.0-2.2.6/EntityFrameworkCore.Testing.NSubstitute/Extensions/DbQueryExtensions.cs
@@ -13,16 +13,16 @@ namespace EntityFrameworkCore.Testing.NSubstitute.Extensions
     /// <summary>Extensions for the db query type.</summary>
     public static class DbQueryExtensions
     {
-        /// <summary>Creates and sets up a mocked db query.</summary>
+        /// <summary>Creates and sets up a substitute db query.</summary>
         /// <typeparam name="TQuery">The query type.</typeparam>
         /// <param name="dbQuery">The db query to mock.</param>
-        /// <returns>A mocked db query.</returns>
+        /// <returns>A substitute db query.</returns>
         public static DbQuery<TQuery> CreateSubstituteDbQuery<TQuery>(this DbQuery<TQuery> dbQuery) 
             where TQuery : class
         {
             EnsureArgument.IsNotNull(dbQuery, nameof(dbQuery));
 
-            var mockedDbQuery = (DbQuery<TQuery>)
+            var substituteDbQuery = (DbQuery<TQuery>)
                 Substitute.For(new[] {
                         typeof(DbQuery<TQuery>),
                         typeof(IAsyncEnumerableAccessor<TQuery>),
@@ -36,24 +36,24 @@ namespace EntityFrameworkCore.Testing.NSubstitute.Extensions
 
             var queryable = new List<TQuery>().AsQueryable();
 
-            ((IAsyncEnumerableAccessor<TQuery>) mockedDbQuery).AsyncEnumerable.Returns(queryable.ToAsyncEnumerable());
-            ((IQueryable<TQuery>) mockedDbQuery).ElementType.Returns(queryable.ElementType);
-            ((IQueryable<TQuery>) mockedDbQuery).Expression.Returns(queryable.Expression);
-            ((IEnumerable) mockedDbQuery).GetEnumerator().Returns(callInfo => queryable.GetEnumerator());
-            ((IEnumerable<TQuery>) mockedDbQuery).GetEnumerator().Returns(callInfo => queryable.GetEnumerator());
+            ((IAsyncEnumerableAccessor<TQuery>) substituteDbQuery).AsyncEnumerable.Returns(callInfo => queryable.ToAsyncEnumerable());
+            ((IQueryable<TQuery>) substituteDbQuery).ElementType.Returns(callInfo => queryable.ElementType);
+            ((IQueryable<TQuery>) substituteDbQuery).Expression.Returns(callInfo => queryable.Expression);
+            ((IEnumerable) substituteDbQuery).GetEnumerator().Returns(callInfo => queryable.GetEnumerator());
+            ((IEnumerable<TQuery>) substituteDbQuery).GetEnumerator().Returns(callInfo => queryable.GetEnumerator());
 
-            ((IInfrastructure<IServiceProvider>) mockedDbQuery).Instance.Returns(((IInfrastructure<IServiceProvider>) mockedDbQuery).Instance);
+            ((IInfrastructure<IServiceProvider>) substituteDbQuery).Instance.Returns(callInfo => ((IInfrastructure<IServiceProvider>) substituteDbQuery).Instance);
 
-            var mockedQueryProvider = ((IQueryable<TQuery>) mockedDbQuery).Provider.CreateSubstituteQueryProvider(new List<TQuery>());
-            ((IQueryable<TQuery>) mockedDbQuery).Provider.Returns(mockedQueryProvider);
+            var substituteQueryProvider = ((IQueryable<TQuery>) substituteDbQuery).Provider.CreateSubstituteQueryProvider(new List<TQuery>());
+            ((IQueryable<TQuery>) substituteDbQuery).Provider.Returns(callInfo => substituteQueryProvider);
 
-            return mockedDbQuery;
+            return substituteDbQuery;
         }
 
-        /// <summary>Creates and sets up a mocked db query.</summary>
+        /// <summary>Creates and sets up a substitute db query.</summary>
         /// <typeparam name="TQuery">The query type.</typeparam>
         /// <param name="dbQuery">The db query to mock.</param>
-        /// <returns>A mocked db query.</returns>
+        /// <returns>A substitute db query.</returns>
         [Obsolete("This will be removed in a future version. Use DbQueryExtensions.CreateDbQuerySubstitute instead.")]
         public static DbQuery<TQuery> CreateMock<TQuery>(this DbQuery<TQuery> dbQuery)
             where TQuery : class
@@ -61,105 +61,105 @@ namespace EntityFrameworkCore.Testing.NSubstitute.Extensions
             return dbQuery.CreateSubstituteDbQuery();
         }
 
-        /// <summary>Adds an item to the end of the mocked db query source.</summary>
+        /// <summary>Adds an item to the end of the substitute db query source.</summary>
         /// <typeparam name="TQuery">The query type.</typeparam>
-        /// <param name="mockedDbQuery">The mocked db query.</param>
-        /// <param name="item">The item to be added to the end of the mocked db query source.</param>
+        /// <param name="substituteDbQuery">The substitute db query.</param>
+        /// <param name="item">The item to be added to the end of the substitute db query source.</param>
         [Obsolete(
             "This has been replaced by DbQueryExtensions.AddToReadOnlySource to avoid conflicts with the " +
             "EntityFrameworkCore 3.0.0 read only set Add method.")
         ]
-        public static void Add<TQuery>(this DbQuery<TQuery> mockedDbQuery, TQuery item) 
+        public static void Add<TQuery>(this DbQuery<TQuery> substituteDbQuery, TQuery item) 
             where TQuery : class
         {
-            mockedDbQuery.AddToReadOnlySource(item);
+            substituteDbQuery.AddToReadOnlySource(item);
         }
 
-        /// <summary>Adds an item to the end of the mocked db query source.</summary>
+        /// <summary>Adds an item to the end of the substitute db query source.</summary>
         /// <typeparam name="TQuery">The query type.</typeparam>
-        /// <param name="mockedDbQuery">The mocked db query.</param>
-        /// <param name="item">The item to be added to the end of the mocked db query source.</param>
-        public static void AddToReadOnlySource<TQuery>(this DbQuery<TQuery> mockedDbQuery, TQuery item) 
+        /// <param name="substituteDbQuery">The substitute db query.</param>
+        /// <param name="item">The item to be added to the end of the substitute db query source.</param>
+        public static void AddToReadOnlySource<TQuery>(this DbQuery<TQuery> substituteDbQuery, TQuery item) 
             where TQuery : class
         {
-            EnsureArgument.IsNotNull(mockedDbQuery, nameof(mockedDbQuery));
+            EnsureArgument.IsNotNull(substituteDbQuery, nameof(substituteDbQuery));
             EnsureArgument.IsNotNull(item, nameof(item));
 
-            var list = mockedDbQuery.ToList();
+            var list = substituteDbQuery.ToList();
             list.Add(item);
             var queryable = list.AsQueryable();
 
-            mockedDbQuery.SetSource(queryable);
+            substituteDbQuery.SetSource(queryable);
         }
 
-        /// <summary>Adds the items of the specified sequence to the end of the mocked db query source.</summary>
+        /// <summary>Adds the items of the specified sequence to the end of the substitute db query source.</summary>
         /// <typeparam name="TQuery">The query type.</typeparam>
-        /// <param name="mockedDbQuery">The mocked db query.</param>
-        /// <param name="items">The sequence whose items should be added to the end of the mocked db query source.</param>
+        /// <param name="substituteDbQuery">The substitute db query.</param>
+        /// <param name="items">The sequence whose items should be added to the end of the substitute db query source.</param>
         [Obsolete(
             "This has been replaced by DbQueryExtensions.AddRangeToReadOnlySource to avoid conflicts with the " +
             "EntityFrameworkCore 3.0.0 read only set AddRange method.")
         ]
-        public static void AddRange<TQuery>(this DbQuery<TQuery> mockedDbQuery, IEnumerable<TQuery> items) 
+        public static void AddRange<TQuery>(this DbQuery<TQuery> substituteDbQuery, IEnumerable<TQuery> items) 
             where TQuery : class
         {
-            mockedDbQuery.AddRangeToReadOnlySource(items);
+            substituteDbQuery.AddRangeToReadOnlySource(items);
         }
 
-        /// <summary>Adds the items of the specified sequence to the end of the mocked db query source.</summary>
+        /// <summary>Adds the items of the specified sequence to the end of the substitute db query source.</summary>
         /// <typeparam name="TQuery">The query type.</typeparam>
-        /// <param name="mockedDbQuery">The mocked db query.</param>
-        /// <param name="items">The sequence whose items should be added to the end of the mocked db query source.</param>
-        public static void AddRangeToReadOnlySource<TQuery>(this DbQuery<TQuery> mockedDbQuery, IEnumerable<TQuery> items) 
+        /// <param name="substituteDbQuery">The substitute db query.</param>
+        /// <param name="items">The sequence whose items should be added to the end of the substitute db query source.</param>
+        public static void AddRangeToReadOnlySource<TQuery>(this DbQuery<TQuery> substituteDbQuery, IEnumerable<TQuery> items) 
             where TQuery : class
         {
-            EnsureArgument.IsNotNull(mockedDbQuery, nameof(mockedDbQuery));
+            EnsureArgument.IsNotNull(substituteDbQuery, nameof(substituteDbQuery));
             EnsureArgument.IsNotNull(items, nameof(items));
             EnsureArgument.IsNotEmpty(items, nameof(items));
 
-            var list = mockedDbQuery.ToList();
+            var list = substituteDbQuery.ToList();
             list.AddRange(items);
             var queryable = list.AsQueryable();
 
-            mockedDbQuery.SetSource(queryable);
+            substituteDbQuery.SetSource(queryable);
         }
 
-        /// <summary>Removes all items from the mocked db query source.</summary>
+        /// <summary>Removes all items from the substitute db query source.</summary>
         /// <typeparam name="TQuery">The query type.</typeparam>
-        /// <param name="mockedDbQuery">The mocked db query.</param>
+        /// <param name="substituteDbQuery">The substitute db query.</param>
         [Obsolete("This has been replaced by DbQueryExtensions.ClearReadOnlySource.")]
-        public static void Clear<TQuery>(this DbQuery<TQuery> mockedDbQuery) 
+        public static void Clear<TQuery>(this DbQuery<TQuery> substituteDbQuery) 
             where TQuery : class
         {
-            mockedDbQuery.ClearReadOnlySource();
+            substituteDbQuery.ClearReadOnlySource();
         }
 
-        /// <summary>Removes all items from the mocked db query source.</summary>
+        /// <summary>Removes all items from the substitute db query source.</summary>
         /// <typeparam name="TQuery">The query type.</typeparam>
-        /// <param name="mockedDbQuery">The mocked db query.</param>
-        public static void ClearReadOnlySource<TQuery>(this DbQuery<TQuery> mockedDbQuery) 
+        /// <param name="substituteDbQuery">The substitute db query.</param>
+        public static void ClearReadOnlySource<TQuery>(this DbQuery<TQuery> substituteDbQuery) 
             where TQuery : class
         {
-            EnsureArgument.IsNotNull(mockedDbQuery, nameof(mockedDbQuery));
+            EnsureArgument.IsNotNull(substituteDbQuery, nameof(substituteDbQuery));
 
-            mockedDbQuery.SetSource(new List<TQuery>());
+            substituteDbQuery.SetSource(new List<TQuery>());
         }
 
-        internal static void SetSource<TQuery>(this DbQuery<TQuery> mockedDbQuery, IEnumerable<TQuery> source) 
+        internal static void SetSource<TQuery>(this DbQuery<TQuery> substituteDbQuery, IEnumerable<TQuery> source) 
             where TQuery : class
         {
-            EnsureArgument.IsNotNull(mockedDbQuery, nameof(mockedDbQuery));
+            EnsureArgument.IsNotNull(substituteDbQuery, nameof(substituteDbQuery));
             EnsureArgument.IsNotNull(source, nameof(source));
 
             var queryable = source.AsQueryable();
 
-            ((IAsyncEnumerableAccessor<TQuery>) mockedDbQuery).AsyncEnumerable.Returns(queryable.ToAsyncEnumerable());
-            ((IQueryable<TQuery>) mockedDbQuery).ElementType.Returns(queryable.ElementType);
-            ((IQueryable<TQuery>) mockedDbQuery).Expression.Returns(queryable.Expression);
-            ((IEnumerable) mockedDbQuery).GetEnumerator().Returns(callInfo => queryable.GetEnumerator());
-            ((IEnumerable<TQuery>) mockedDbQuery).GetEnumerator().Returns(callInfo => queryable.GetEnumerator());
+            ((IAsyncEnumerableAccessor<TQuery>) substituteDbQuery).AsyncEnumerable.Returns(callInfo => queryable.ToAsyncEnumerable());
+            ((IQueryable<TQuery>) substituteDbQuery).ElementType.Returns(callInfo => queryable.ElementType);
+            ((IQueryable<TQuery>) substituteDbQuery).Expression.Returns(callInfo => queryable.Expression);
+            ((IEnumerable) substituteDbQuery).GetEnumerator().Returns(callInfo => queryable.GetEnumerator());
+            ((IEnumerable<TQuery>) substituteDbQuery).GetEnumerator().Returns(callInfo => queryable.GetEnumerator());
 
-            var provider = ((IQueryable<TQuery>) mockedDbQuery).Provider;
+            var provider = ((IQueryable<TQuery>) substituteDbQuery).Provider;
             ((AsyncQueryProvider<TQuery>) provider).SetSource(queryable);
         }
     }
