7aaa10dda33a2b104ccc3123cdb5920deb71203f
Lee Anderson <arrgeeveelee@gmail.com>
2019-12-20 19:04:08 +0800

    Moved obsolete methods to their own file

---
+++ src/EntityFrameworkCore 3.0.0/EntityFrameworkCore.Testing.Moq/Extensions/ReadOnlyDbSetExtensions.Obsolete.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Testing.Moq.Extensions
+{
+    /// <summary>Extensions for the db set type.</summary>
+    public static partial class ReadOnlyDbSetExtensions
+    {
+        /// <summary>Creates and sets up a mocked db query.</summary>
+        /// <typeparam name="TQuery">The query type.</typeparam>
+        /// <param name="dbQuery">The db query to mock.</param>
+        /// <returns>A mocked readonly db query.</returns>
+        [Obsolete("This method will remain until EntityFrameworkCore no longer supports the DbQuery<TQuery> type. Use ReadOnlyDbSetExtensions.CreateMockedReadOnlyDbSet instead.")]
+        public static DbQuery<TQuery> CreateMockedDbQuery<TQuery>(this DbQuery<TQuery> dbQuery)
+            where TQuery : class
+        {
+            var mockedReadOnlyDbSet = dbQuery.CreateMockedReadOnlyDbSet();
+            return (DbQuery<TQuery>) mockedReadOnlyDbSet;
+        }
+
+        /// <summary>Creates and sets up a mocked db query.</summary>
+        /// <typeparam name="TQuery">The query type.</typeparam>
+        /// <param name="dbQuery">The db query to mock.</param>
+        /// <returns>A mocked readonly db query.</returns>
+        [Obsolete("This will be removed in a future version. Use ReadOnlyDbSetExtensions.CreateMockedDbQuery instead.")]
+        public static DbQuery<TQuery> CreateReadOnlyMock<TQuery>(this DbQuery<TQuery> dbQuery)
+            where TQuery : class
+        {
+            return dbQuery.CreateMockedDbQuery();
+        }
+
+        /// <summary>Creates and sets up a mocked readonly db set.</summary>
+        /// <typeparam name="TEntity">The entity type.</typeparam>
+        /// <param name="readOnlyDbSet">The readonly db set to mock.</param>
+        /// <returns>A mocked readonly db set.</returns>
+        [Obsolete("This will be removed in a future version. Use ReadOnlyDbSetExtensions.CreateMockedReadOnlyDbSet instead.")]
+        public static DbSet<TEntity> CreateReadOnlyMock<TEntity>(this DbSet<TEntity> readOnlyDbSet)
+            where TEntity : class
+        {
+            return readOnlyDbSet.CreateMockedReadOnlyDbSet();
+        }
+    }
+}
---
+++ src/EntityFrameworkCore 3.0.0/EntityFrameworkCore.Testing.NSubstitute/Extensions/ReadOnlyDbSetExtensions.Obsolete.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Testing.NSubstitute.Extensions
+{
+    /// <summary>Extensions for read-only db sets.</summary>
+    public static partial class ReadOnlyDbSetExtensions
+    {
+        /// <summary>Creates and sets up a substitute db query.</summary>
+        /// <typeparam name="TQuery">The query type.</typeparam>
+        /// <param name="dbQuery">The db query to mock.</param>
+        /// <returns>A substitute readonly db query.</returns>
+        [Obsolete("This method will remain until EntityFrameworkCore no longer supports the DbQuery<TQuery> type. Use ReadOnlyDbSetExtensions.CreateSubstituteReadOnlyDbSet instead.")]
+        public static DbQuery<TQuery> CreateSubstituteDbQuery<TQuery>(this DbQuery<TQuery> dbQuery)
+            where TQuery : class
+        {
+            var substituteReadOnlyDbSet = dbQuery.CreateSubstituteReadOnlyDbSet();
+            return (DbQuery<TQuery>) substituteReadOnlyDbSet;
+        }
+    }
+}
