8f65e1c2f6350f0ac848b48a47660bf5c2dbd405
Lee Anderson <arrgeeveelee@gmail.com>
2019-12-28 14:03:40 +0800

    Added tests for issue 569237159

---
+++ src/EntityFrameworkCore 3.0.0/EntityFrameworkCore.Testing.NSubstitute.Tests/Issue569237159Tests.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using EntityFrameworkCore.Testing.Common.Tests;
+using EntityFrameworkCore.Testing.NSubstitute.Extensions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace EntityFrameworkCore.Testing.NSubstitute.Tests
+{
+    public class Issue569237159Tests
+    {
+        [SetUp]
+        public virtual void SetUp()
+        {
+            //LoggerHelper.LoggerFactory.AddConsole(LogLevel.Debug);
+        }
+
+        [Test]
+        public async Task ExecuteSqlRawAsync_SpecifiedSqlAndSqlParameter_ReturnsExpectedResultAndSetsOutputParameterValue()
+        {
+            var mockedDbContext = Create.MockedDbContextFor<TestDbContext>();
+            mockedDbContext.AddExecuteSqlRawResult(string.Empty, new List<object>(), -1, (sql, parameters) =>
+            {
+                ((SqlParameter) parameters.ElementAt(0)).Value = "Cookie";
+            });
+
+            var outcomeParam = new SqlParameter("Outcome", SqlDbType.VarChar, 500) {Direction = ParameterDirection.Output};
+            var result = await mockedDbContext.Database.ExecuteSqlRawAsync(@"EXEC [GiveMeCookie] @Outcome = @Outcome OUT", outcomeParam);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(-1, result);
+                Assert.That(outcomeParam.Value.ToString(), Is.EqualTo("Cookie"));
+            });
+        }
+
+        [Test]
+        public void GiveMeCookie_SetsOutputParameterValue()
+        {
+            var mockedDbContext = Create.MockedDbContextFor<TestDbContext>();
+            mockedDbContext.AddExecuteSqlRawResult(string.Empty, new List<object>(), -1, (sql, parameters) =>
+            {
+                ((SqlParameter) parameters.ElementAt(0)).Value = "Cookie";
+            });
+
+            var service = new MyService(mockedDbContext);
+
+            Assert.That(service.GiveMeCookie().Result, Is.EqualTo("Cookie"));
+        }
+
+        public class MyService
+        {
+            private readonly DbContext _context;
+
+            public MyService(DbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<string> GiveMeCookie()
+            {
+                var outcomeParam = new SqlParameter("Outcome", SqlDbType.VarChar, 500) {Direction = ParameterDirection.Output};
+                await _context.Database.ExecuteSqlRawAsync(@"EXEC [GiveMeCookie] @Outcome = @Outcome OUT", outcomeParam);
+                return outcomeParam.Value.ToString();
+            }
+        }
+    }
+}
