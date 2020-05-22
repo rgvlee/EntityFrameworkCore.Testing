47e8136872d24b30a6d2d363f7a6c94005e4d0a2
rgvlee <lee@rgvlee.com>
2020-05-22 21:06:52 +0800

    Added tests for issue #4

---
+++ src/EntityFrameworkCore 2.1.0-2.2.6/EntityFrameworkCore.Testing.Moq.Tests/Issue4Tests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using EntityFrameworkCore.Testing.Common.Helpers;
+using EntityFrameworkCore.Testing.Common.Tests;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+
+namespace EntityFrameworkCore.Testing.Moq.Tests
+{
+    public class Issue4Tests
+    {
+        [SetUp]
+        public virtual void SetUp()
+        {
+            LoggerHelper.LoggerFactory.AddConsole(LogLevel.Debug);
+        }
+
+        [Test]
+        public void AsQueryable_Set_ReturnsIQueryableOfTWithMockedQueryProvider()
+        {
+            var mockedDbContext = Create.MockedDbContextFor<TestDbContext>();
+
+            var mockedSetAsQueryable = mockedDbContext.TestEntities.AsQueryable();
+
+            var asyncProvider = mockedSetAsQueryable.Provider as IAsyncQueryProvider;
+            Assert.That(asyncProvider, Is.Not.Null);
+        }
+
+        [Test]
+        public async Task AsQueryableThenWhereThenSingleOrDefaultAsync_WhereOperationReturnsTrue_ReturnsSingleEntity()
+        {
+            var fixture = new Fixture();
+            var entities = fixture.CreateMany<TestEntity>().ToList();
+            var entityToFind = entities.ElementAt(1);
+            var mockedDbContext = Create.MockedDbContextFor<TestDbContext>();
+            var mockedSet = mockedDbContext.TestEntities;
+            mockedSet.AddRange(entities);
+            mockedDbContext.SaveChanges();
+
+            var result = await mockedSet.AsQueryable().Where(x => x.Guid.Equals(entityToFind.Guid)).SingleOrDefaultAsync();
+
+            Assert.That(result, Is.EqualTo(entityToFind));
+        }
+
+        [Test]
+        public async Task AsQueryableThenWhereThenSingleOrDefaultAsync_WhereOperationReturnsFalse_ReturnsDefault()
+        {
+            var fixture = new Fixture();
+            var entities = fixture.CreateMany<TestEntity>().ToList();
+            var mockedDbContext = Create.MockedDbContextFor<TestDbContext>();
+            var mockedSet = mockedDbContext.TestEntities;
+            mockedSet.AddRange(entities);
+            mockedDbContext.SaveChanges();
+
+            var result = await mockedSet.AsQueryable().Where(x => x.Guid.Equals(Guid.NewGuid())).SingleOrDefaultAsync();
+
+            Assert.That(result, Is.Null);
+        }
+    }
+}
