b7ce09037bab32c8418bf52d797e23f67fad404f
rgvlee <lee@rgvlee.com>
2020-09-19 20:37:24 +0800

    Reinstated the async enumerator Added async enumerable support for projections

--- src/EntityFrameworkCore 3.0.0/EntityFrameworkCore.Testing.Common/AsyncEnumerable.cs
+++ src/EntityFrameworkCore 3.0.0/EntityFrameworkCore.Testing.Common/AsyncEnumerable.cs
@@ -4,29 +4,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
-using EntityFrameworkCore.Testing.Common.Extensions;
 
 namespace EntityFrameworkCore.Testing.Common
 {
-    public class AsyncEnumerable<T> : IAsyncEnumerable<T>, IOrderedQueryable<T>, IEnumerable<T>, IEnumerable, IOrderedQueryable, IQueryable, IQueryable<T>
+    public class AsyncEnumerable<T> : IAsyncEnumerable<T>, IOrderedQueryable<T>
     {
         private readonly IEnumerable<T> _enumerable;
-        private readonly IQueryable<T> _queryable;
 
         public AsyncEnumerable(IEnumerable<T> enumerable)
         {
             _enumerable = enumerable;
+            Expression = enumerable.AsQueryable().Expression;
+            Provider = new AsyncQueryProvider<T>(_enumerable);
+        }
 
-            _queryable = _enumerable.AsQueryable();
-
-            ElementType = _queryable.ElementType;
-            Expression = _queryable.Expression;
-            Provider = new AsyncQueryProvider<T>(_queryable);
+        public AsyncEnumerable(Expression expression)
+        {
+            _enumerable = Expression.Lambda<Func<IEnumerable<T>>>(expression, null).Compile()();
+            Expression = expression;
+            Provider = new AsyncQueryProvider<T>(_enumerable);
         }
 
         public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = new CancellationToken())
         {
-            return _queryable.ToAsyncEnumerable().GetAsyncEnumerator(cancellationToken);
+            return new AsyncEnumerator<T>(_enumerable);
         }
 
         IEnumerator<T> IEnumerable<T>.GetEnumerator()
@@ -39,7 +40,7 @@ namespace EntityFrameworkCore.Testing.Common
             return _enumerable.GetEnumerator();
         }
 
-        public Type ElementType { get; }
+        public Type ElementType => typeof(T);
         public Expression Expression { get; }
         public IQueryProvider Provider { get; }
     }
---
+++ src/EntityFrameworkCore 3.0.0/EntityFrameworkCore.Testing.Common/AsyncEnumerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Testing.Common
+{
+    public class AsyncEnumerator<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> _enumerator;
+
+        public AsyncEnumerator(IEnumerable<T> enumerable)
+        {
+            _enumerator = enumerable.GetEnumerator();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            return new ValueTask();
+        }
+
+        public ValueTask<bool> MoveNextAsync()
+        {
+            return new ValueTask<bool>(_enumerator.MoveNext());
+        }
+
+        public T Current => _enumerator.Current;
+    }
+}
