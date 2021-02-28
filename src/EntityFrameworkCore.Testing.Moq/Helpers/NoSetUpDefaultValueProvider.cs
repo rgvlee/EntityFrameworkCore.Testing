d6789e057638a1c7757805a0e5b70e7e81875829
rgvlee <lee@rgvlee.com>
2021-02-28 10:50:41 +0800

    Tidied up no set up provider

--- src/EntityFrameworkCore.Testing.Moq/Helpers/NoSetUpDefaultValueProvider.cs
+++ src/EntityFrameworkCore.Testing.Moq/Helpers/NoSetUpDefaultValueProvider.cs
@@ -19,18 +19,17 @@ namespace EntityFrameworkCore.Testing.Moq.Helpers
     {
         private static readonly ILogger<NoSetUpDefaultValueProvider<TDbContext>> Logger = LoggingHelper.CreateLogger<NoSetUpDefaultValueProvider<TDbContext>>();
 
-        private readonly TDbContext _dbContext;
-
         private readonly List<IEntityType> _allModelEntityTypes;
 
-        private readonly List<PropertyInfo> _dbContextModelProperties;
+        private readonly TDbContext _dbContext;
+
+        private readonly List<PropertyInfo> _dbContextModelEntityProperties;
 
         public NoSetUpDefaultValueProvider(TDbContext dbContext)
         {
             _dbContext = dbContext;
             _allModelEntityTypes = _dbContext.Model.GetEntityTypes().ToList();
-            _dbContextModelProperties = _dbContext
-                .GetType()
+            _dbContextModelEntityProperties = _dbContext.GetType()
                 .GetProperties()
                 .Where(x => x.PropertyType.IsGenericType &&
                             (x.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>) || x.PropertyType.GetGenericTypeDefinition() == typeof(DbQuery<>)))
@@ -40,29 +39,29 @@ namespace EntityFrameworkCore.Testing.Moq.Helpers
         protected override object GetDefaultValue(Type type, Mock mock)
         {
             var lastInvocation = mock.Invocations.Last();
-            var lastInvocationIsByType = lastInvocation.Method.Name.Equals("Query") || lastInvocation.Method.Name.Equals("Set");
-            var dbContextModelProperty = _dbContextModelProperties.SingleOrDefault(x => x.GetMethod.Name.Equals(lastInvocation.Method.Name));
-            var lastInvocationIsByProperty = dbContextModelProperty != null;
+            var lastInvocationIsByType = lastInvocation.Method.Name.Equals("Set") || lastInvocation.Method.Name.Equals("Query");
+            var dbContextModelEntityProperty = _dbContextModelEntityProperties.SingleOrDefault(x => x.GetMethod.Name.Equals(lastInvocation.Method.Name));
+            var lastInvocationIsByProperty = dbContextModelEntityProperty != null;
 
             if (lastInvocationIsByType || lastInvocationIsByProperty)
             {
-                var setType = lastInvocationIsByType ? lastInvocation.Method.GetGenericArguments().Single() : dbContextModelProperty.PropertyType.GetGenericArguments().Single();
+                var setType = lastInvocationIsByType
+                    ? lastInvocation.Method.GetGenericArguments().Single()
+                    : dbContextModelEntityProperty.PropertyType.GetGenericArguments().Single();
 
                 Logger.LogDebug("Setting up setType: '{setType}'", setType);
 
                 var entityType = _allModelEntityTypes.SingleOrDefault(x => x.ClrType.Equals(setType));
                 if (entityType == null)
                 {
-                    throw new InvalidOperationException(
-                        string.Format(ExceptionMessages.CannotCreateDbSetTypeNotIncludedInModel,
+                    throw new InvalidOperationException(string.Format(ExceptionMessages.CannotCreateDbSetTypeNotIncludedInModel,
                         lastInvocation.Method.GetGenericArguments().Single().Name));
                 }
 
-                var setUpDbSetForMethod = typeof(NoSetUpDefaultValueProvider<TDbContext>)
-                    .GetMethods(BindingFlags.Instance | BindingFlags.NonPublic)
-                    .Single(x => x.Name.Equals(entityType.FindPrimaryKey() != null ? "SetUpDbSetFor" : "SetUpReadOnlyDbSetFor"));
+                var setUpModelEntityMethod = typeof(NoSetUpDefaultValueProvider<TDbContext>).GetMethods(BindingFlags.Instance | BindingFlags.NonPublic)
+                    .Single(x => x.Name.Equals(entityType.FindPrimaryKey() != null ? "SetUpModelEntity" : "SetUpReadOnlyModelEntity"));
 
-                setUpDbSetForMethod.MakeGenericMethod(setType).Invoke(this, new[] { mock });
+                setUpModelEntityMethod.MakeGenericMethod(setType).Invoke(this, new[] { mock });
 
                 return lastInvocation.Method.Invoke(mock.Object, null);
             }
@@ -70,7 +69,7 @@ namespace EntityFrameworkCore.Testing.Moq.Helpers
             return type.GetDefaultValue();
         }
 
-        internal void SetUpDbSetFor<TEntity>(Mock<TDbContext> dbContextMock) where TEntity : class
+        private void SetUpModelEntity<TEntity>(Mock<TDbContext> dbContextMock) where TEntity : class
         {
             var mockedDbSet = _dbContext.Set<TEntity>().CreateMockedDbSet();
 
@@ -78,12 +77,12 @@ namespace EntityFrameworkCore.Testing.Moq.Helpers
 
             if (property != null)
             {
-                var expression = ExpressionHelper.CreatePropertyExpression<TDbContext, DbSet<TEntity>>(property);
-                dbContextMock.Setup(expression).Returns(mockedDbSet);
+                var propertyExpression = ExpressionHelper.CreatePropertyExpression<TDbContext, DbSet<TEntity>>(property);
+                dbContextMock.Setup(propertyExpression).Returns(mockedDbSet);
             }
             else
             {
-                Logger.LogDebug($"Could not find a DbContext property for type '{typeof(TEntity)}'");
+                Logger.LogDebug("Could not find a DbContext property for type '{type}'", typeof(TEntity));
             }
 
             dbContextMock.Setup(m => m.Set<TEntity>()).Returns(mockedDbSet);
@@ -110,7 +109,7 @@ namespace EntityFrameworkCore.Testing.Moq.Helpers
             dbContextMock.Setup(m => m.Update(It.IsAny<TEntity>())).Returns((TEntity providedEntity) => _dbContext.Update(providedEntity));
         }
 
-        internal void SetUpReadOnlyDbSetFor<TEntity>(Mock<TDbContext> dbContextMock) where TEntity : class
+        private void SetUpReadOnlyModelEntity<TEntity>(Mock<TDbContext> dbContextMock) where TEntity : class
         {
             var mockedReadOnlyDbSet = _dbContext.Set<TEntity>().CreateMockedReadOnlyDbSet();
 
@@ -119,22 +118,22 @@ namespace EntityFrameworkCore.Testing.Moq.Helpers
             {
                 if (property.PropertyType == typeof(DbSet<TEntity>))
                 {
-                    var setExpression = ExpressionHelper.CreatePropertyExpression<TDbContext, DbSet<TEntity>>(property);
-                    dbContextMock.Setup(setExpression).Returns(mockedReadOnlyDbSet);
+                    var propertyExpression = ExpressionHelper.CreatePropertyExpression<TDbContext, DbSet<TEntity>>(property);
+                    dbContextMock.Setup(propertyExpression).Returns(mockedReadOnlyDbSet);
                 }
                 else
                 {
-                    var setExpression = ExpressionHelper.CreatePropertyExpression<TDbContext, DbQuery<TEntity>>(property);
-                    dbContextMock.Setup(setExpression).Returns(mockedReadOnlyDbSet);
+                    var propertyExpression = ExpressionHelper.CreatePropertyExpression<TDbContext, DbQuery<TEntity>>(property);
+                    dbContextMock.Setup(propertyExpression).Returns(mockedReadOnlyDbSet);
                 }
-
-                dbContextMock.Setup(m => m.Set<TEntity>()).Returns(mockedReadOnlyDbSet);
-                dbContextMock.Setup(m => m.Query<TEntity>()).Returns(mockedReadOnlyDbSet);
             }
             else
             {
-                Logger.LogDebug($"Could not find a DbContext property for type '{typeof(TEntity)}'");
+                Logger.LogDebug("Could not find a DbContext property for type '{type}'", typeof(TEntity));
             }
+
+            dbContextMock.Setup(m => m.Set<TEntity>()).Returns(mockedReadOnlyDbSet);
+            dbContextMock.Setup(m => m.Query<TEntity>()).Returns(mockedReadOnlyDbSet);
         }
     }
 }
