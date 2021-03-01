8b3c365ea8dacb617a9051162fa92ea175de0d7d
rgvlee <lee@rgvlee.com>
2021-03-01 09:43:00 +0800

    Removed log message interpolation Moved common methods to common Async class sync with 3.x Cleaned up resources

--- src/EntityFrameworkCore.Testing.Common/AsyncQueryProvider.cs
+++ src/EntityFrameworkCore.Testing.Common/AsyncQueryProvider.cs
@@ -8,13 +8,14 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using Microsoft.Extensions.Logging;
 using rgvlee.Core.Common.Helpers;
+using ProjectExpressionHelper = EntityFrameworkCore.Testing.Common.Helpers.ExpressionHelper;
 
 namespace EntityFrameworkCore.Testing.Common
 {
     /// <inheritdoc />
     public class AsyncQueryProvider<T> : IAsyncQueryProvider
     {
-        private static readonly ILogger Logger = LoggingHelper.CreateLogger(typeof(AsyncQueryProvider<T>));
+        private static readonly ILogger Logger = LoggingHelper.CreateLogger<AsyncQueryProvider<T>>();
 
         public AsyncQueryProvider(IEnumerable<T> enumerable)
         {
@@ -27,19 +28,26 @@ namespace EntityFrameworkCore.Testing.Common
         public virtual IQueryable<T> Source { get; set; }
 
         /// <inheritdoc />
+        /// <remarks>In this implementation it is just a wrapper for <see cref="AsyncQueryProvider{T}.CreateQuery{T}(Expression)" /></remarks>
         public virtual IQueryable CreateQuery(Expression expression)
         {
+            Logger.LogDebug("CreateQuery: invoked");
+
             //Handles cases where we are projecting to another type
-            if (expression is MethodCallExpression methodCallExpression)
+            if (expression is MethodCallExpression mce)
             {
-                var returnType = methodCallExpression.Method.ReturnType;
+                var returnType = mce.Method.ReturnType;
                 if (returnType.GetGenericTypeDefinition() != typeof(IQueryable<>))
                 {
                     throw new InvalidOperationException($"Expected IQueryable<>; actual {returnType.FullName}");
                 }
 
+                var createQueryMethod = typeof(IQueryProvider).GetMethods().Single(x => x.Name.Equals(nameof(IQueryProvider.CreateQuery)) && x.IsGenericMethod);
+
+                var createQueryResult = createQueryMethod.MakeGenericMethod(returnType.GetGenericArguments().Single()).Invoke(this, new[] { expression });
+
                 return (IQueryable) Activator.CreateInstance(typeof(AsyncEnumerable<>).GetGenericTypeDefinition().MakeGenericType(returnType.GetGenericArguments().Single()),
-                    expression);
+                    createQueryResult);
             }
 
             return CreateQuery<T>(expression);
@@ -48,89 +56,52 @@ namespace EntityFrameworkCore.Testing.Common
         /// <inheritdoc />
         public virtual IQueryable<TElement> CreateQuery<TElement>(Expression expression)
         {
-            //Why is this done here? Primarily because we can't change the Moq mock interface once we have started using the mocked instance.
-            //It is not possible to set up every possible interface as the method can be invoked with an anonymous type.
-            //We don't lose anything doing it this way. We can still verify invocations and we don't have to duplicate the functionality for each project.
-            if (expression is MethodCallExpression methodCallExpression)
+            Logger.LogDebug("CreateQuery<TElement>: invoked");
+
+            if (expression is MethodCallExpression mce && mce.Method.Name.Equals("FromSqlOnQueryable"))
             {
-                if (methodCallExpression.Method.Name.Equals(nameof(Queryable.Select)))
-                {
-                    Logger.LogDebug($"{methodCallExpression.Method.Name} invoked; expression: '{methodCallExpression}'");
-
-                    var unaryExpression = (UnaryExpression) methodCallExpression.Arguments[1];
-                    var predicateExpression = unaryExpression.Operand;
-                    if (predicateExpression.Type.GetGenericArguments().ToList().Count.Equals(3))
-                    {
-                        var predicate = ((Expression<Func<T, int, TElement>>) predicateExpression).Compile();
-                        return new AsyncEnumerable<TElement>(Source.ToList().Select((x, i) => predicate(x, i)));
-                    }
-                    else
-                    {
-                        var predicate = ((Expression<Func<T, TElement>>) predicateExpression).Compile();
-                        return new AsyncEnumerable<TElement>(Source.ToList().Select(x => predicate(x)));
-                    }
-                }
-
-                if (methodCallExpression.Method.Name.Equals(nameof(Queryable.SkipWhile)) || methodCallExpression.Method.Name.Equals(nameof(Queryable.TakeWhile)))
-                {
-                    var unaryExpression = (UnaryExpression) methodCallExpression.Arguments[1];
-                    var predicateExpression = unaryExpression.Operand;
-                    if (predicateExpression.Type.GetGenericArguments().ToList().Count.Equals(3))
-                    {
-                        var predicate = ((Expression<Func<TElement, int, bool>>) predicateExpression).Compile();
-                        return methodCallExpression.Method.Name.Equals(nameof(Queryable.SkipWhile))
-                            ? Source.Cast<TElement>().ToList().SkipWhile((x, i) => predicate(x, i)).AsQueryable()
-                            : Source.Cast<TElement>().ToList().TakeWhile((x, i) => predicate(x, i)).AsQueryable();
-                    }
-                    else
-                    {
-                        var predicate = ((Expression<Func<TElement, bool>>) predicateExpression).Compile();
-                        return methodCallExpression.Method.Name.Equals(nameof(Queryable.SkipWhile))
-                            ? Source.Cast<TElement>().ToList().SkipWhile(x => predicate(x)).AsQueryable()
-                            : Source.Cast<TElement>().ToList().TakeWhile(x => predicate(x)).AsQueryable();
-                    }
-                }
+                Logger.LogDebug("CreateQuery: catch all exception invoked");
+                throw new NotSupportedException();
             }
 
-            return new AsyncEnumerable<TElement>(expression);
+            ProjectExpressionHelper.ThrowIfExpressionIsNotSupported(expression);
+
+            return new AsyncEnumerable<TElement>(Source.Provider.CreateQuery<TElement>(EnsureExpressionCanBeResolvedBySourceProvider(expression)));
         }
 
         /// <inheritdoc />
         public virtual object Execute(Expression expression)
         {
-            return Source.Provider.Execute(expression);
+            Logger.LogDebug("Execute: invoked");
+            ProjectExpressionHelper.ThrowIfExpressionIsNotSupported(expression);
+            return Source.Provider.Execute(EnsureExpressionCanBeResolvedBySourceProvider(expression));
         }
 
         /// <inheritdoc />
         public virtual TResult Execute<TResult>(Expression expression)
         {
-            if (expression is MethodCallExpression methodCallExpression)
-            {
-                if (methodCallExpression.Method.Name.Equals(nameof(Queryable.ElementAt)) || methodCallExpression.Method.Name.Equals(nameof(Queryable.ElementAtOrDefault)))
-                {
-                    var mce = methodCallExpression;
-                    var index = (int) ((ConstantExpression) mce.Arguments[1]).Value;
-                    return mce.Method.Name.Equals(nameof(Queryable.ElementAt))
-                        ? Source.Cast<TResult>().ToList().ElementAt(index)
-                        : Source.Cast<TResult>().ToList().ElementAtOrDefault(index);
-                }
-            }
-
-            return Source.Provider.Execute<TResult>(expression);
+            Logger.LogDebug("Execute<TResult>: invoked");
+            ProjectExpressionHelper.ThrowIfExpressionIsNotSupported(expression);
+            return Source.Provider.Execute<TResult>(EnsureExpressionCanBeResolvedBySourceProvider(expression));
         }
 
         /// <inheritdoc />
         public virtual TResult ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
         {
             //TResult is a Task<T>. The provider requires T.
-            return (TResult) typeof(AsyncQueryProvider<T>).GetMethod(nameof(InternalExecuteAsync), BindingFlags.Instance | BindingFlags.NonPublic)
+            return (TResult) typeof(AsyncQueryProvider<T>).GetMethod(nameof(WrapExecuteAsync), BindingFlags.Instance | BindingFlags.NonPublic)
                 .MakeGenericMethod(typeof(TResult).GetGenericArguments())
                 .Invoke(this, new object[] { expression, cancellationToken });
         }
 
-        internal Task<TResult> InternalExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+        private Task<TResult> WrapExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
         {
             return Task.FromResult(Execute<TResult>(expression));
+        }
+
+        private Expression EnsureExpressionCanBeResolvedBySourceProvider(Expression expression)
+        {
+            return expression;
         }
     }
 }
--- src/EntityFrameworkCore.Testing.Common/Helpers/ExpressionHelper.cs
+++ src/EntityFrameworkCore.Testing.Common/Helpers/ExpressionHelper.cs
@@ -1,15 +1,21 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
 using rgvlee.Core.Common.Helpers;
 
 namespace EntityFrameworkCore.Testing.Common.Helpers
 {
     /// <summary>
-    ///     A helper for creating expressions.
+    ///     A helper for expressions.
     /// </summary>
     public static class ExpressionHelper
     {
+        private static readonly ILogger Logger = LoggingHelper.CreateLogger(typeof(ExpressionHelper));
+
         /// <summary>
         ///     Creates a property expression for the specified property.
         /// </summary>
@@ -23,6 +29,88 @@ namespace EntityFrameworkCore.Testing.Common.Helpers
 
             var parameter = Expression.Parameter(typeof(TParameter));
             return Expression.Lambda<Func<TParameter, TProperty>>(Expression.Property(parameter, propertyInfo), parameter);
+        }
+
+        public static bool SqlAndParametersMatchFromSqlExpression(string sql, IEnumerable<object> parameters, MethodCallExpression expression)
+        {
+            EnsureArgument.IsNotNull(expression, nameof(expression));
+            EnsureArgument.IsNotNull(parameters, nameof(parameters));
+
+            var result = expression.Method.Name.Equals("FromSqlOnQueryable") &&
+                         SqlMatchesFromSqlExpression(sql, expression) &&
+                         ParameterMatchingHelper.DoInvocationParametersMatchSetUpParameters(parameters, (object[]) ((ConstantExpression) expression.Arguments[2]).Value);
+
+            Logger.LogDebug("Match? {result}", result);
+
+            return result;
+        }
+
+        private static bool SqlMatchesFromSqlExpression(string sql, MethodCallExpression expression)
+        {
+            EnsureArgument.IsNotNull(expression, nameof(expression));
+
+            var expressionSql = (string) ((ConstantExpression) expression.Arguments[1]).Value;
+            var parts = new List<string>();
+            parts.Add($"Invocation sql: '{expressionSql}'");
+            parts.Add($"Set up sql: '{sql}'");
+            Logger.LogDebug(string.Join(Environment.NewLine, parts));
+
+            var result = expressionSql.Contains(sql, StringComparison.CurrentCultureIgnoreCase);
+
+            Logger.LogDebug("Match? {result}", result);
+
+            return result;
+        }
+
+        public static string StringifyFromSqlExpression(MethodCallExpression expression)
+        {
+            EnsureArgument.IsNotNull(expression, nameof(expression));
+
+            var expressionSql = (string) ((ConstantExpression) expression.Arguments[1]).Value;
+            var expressionParameters = (object[]) ((ConstantExpression) expression.Arguments[2]).Value;
+            var parts = new List<string>();
+            parts.Add($"Invocation sql: '{expressionSql}'");
+            parts.Add("Invocation Parameters:");
+            parts.Add(ParameterMatchingHelper.StringifyParameters(expressionParameters));
+            return string.Join(Environment.NewLine, parts);
+        }
+
+        public static void ThrowIfExpressionIsNotSupported(Expression expression)
+        {
+            if (expression is MethodCallExpression mce)
+            {
+                Logger.LogDebug("{methodName} invoked; expression: '{expression}'", mce.Method.Name, mce);
+
+                if (mce.Method.Name.Equals(nameof(Queryable.ElementAt)))
+                {
+                    throw new InvalidOperationException();
+                }
+
+                if (mce.Method.Name.Equals(nameof(Queryable.ElementAtOrDefault)))
+                {
+                    throw new InvalidOperationException();
+                }
+
+                if (mce.Method.Name.Equals(nameof(Queryable.Select)))
+                {
+                    var unaryExpression = (UnaryExpression) mce.Arguments[1];
+                    var predicateExpression = unaryExpression.Operand;
+                    if (predicateExpression.Type.GetGenericArguments().ToList().Count.Equals(3))
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+
+                if (mce.Method.Name.Equals(nameof(Queryable.SkipWhile)))
+                {
+                    throw new InvalidOperationException();
+                }
+
+                if (mce.Method.Name.Equals(nameof(Queryable.TakeWhile)))
+                {
+                    throw new InvalidOperationException();
+                }
+            }
         }
     }
 }
