306a231677f0b522e0bed840c849ba9a209a026c
rgvlee <lee@rgvlee.com>
2021-03-28 13:16:00 +0800

    Updated the Moq no set up handler to catch additional method invocations Removed duplicate set ups

---
+++ src/EntityFrameworkCore.Testing.Moq.Tests/Issue49Tests.cs
@@ -0,0 +1,16 @@
+﻿using EntityFrameworkCore.Testing.Common.Tests;
+using NUnit.Framework;
+
+namespace EntityFrameworkCore.Testing.Moq.Tests
+{
+    public class Issue49Tests : Issue49Tests<TestDbContext, TestEntity>
+    {
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+
+            DbContext = Create.MockedDbContextFor<TestDbContext>();
+        }
+    }
+}
