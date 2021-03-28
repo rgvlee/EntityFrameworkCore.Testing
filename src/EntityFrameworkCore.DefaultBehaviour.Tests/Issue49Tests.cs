288ed4259ff51589212708e37ffbc6d1ef4beac8
rgvlee <lee@rgvlee.com>
2021-03-28 15:00:12 +0800

    Applied 3.x changes

---
+++ src/EntityFrameworkCore.DefaultBehaviour.Tests/Issue49Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EntityFrameworkCore.Testing.Common.Tests;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace EntityFrameworkCore.DefaultBehaviour.Tests
+{
+    public class Issue49Tests : Issue49Tests<TestDbContext, TestEntity>
+    {
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+
+            DbContext = new TestDbContext(new DbContextOptionsBuilder<TestDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options);
+        }
+    }
+}
