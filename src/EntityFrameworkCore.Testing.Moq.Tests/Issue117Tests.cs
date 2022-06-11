47fbec897caea01e28f8184f8992541476d15e9e
Lee Anderson <lee@rgvlee.com>
2022-06-11 15:09:40 +0800

    Applied 4.x changes

---
+++ src/EntityFrameworkCore.Testing.Moq.Tests/Issue117Tests.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFrameworkCore.Testing.Moq.Tests
+{
+    public class Issue117Tests : Common.Tests.Issue117Tests
+    {
+        protected override TestDbContext MockedDbContextFactory()
+        {
+            return Create.MockedDbContextFor<TestDbContext>();
+        }
+    }
+}
