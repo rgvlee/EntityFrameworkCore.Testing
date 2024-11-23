674219d0ee174eda313f200f67b84779da1d3a0b
Lee Anderson <lee.anderson@inxsoftware.com>
2024-11-23 17:16:11 +0800

    Added support for DatabaseFacade.BeginTransactionAsync

---
+++ src/EntityFrameworkCore.Testing.Common.Tests/Issue126Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace EntityFrameworkCore.Testing.Common.Tests;
+
+public abstract class Issue126Tests<TDbContext> : BaseForTests where TDbContext : DbContext
+{
+    protected Func<TDbContext> DbContextFactory;
+
+    [Test]
+    public virtual void BeginTransaction_ReturnsMockTransaction()
+    {
+        using var transaction = DbContextFactory().Database.BeginTransaction();
+        transaction.Should().NotBeNull();
+    }
+
+    [TestCase(true)]
+    [TestCase(false)]
+    public async Task BeginTransactionAsync_ReturnsMockTransaction(bool withCancellationTokenParameter)
+    {
+        await using var transaction = withCancellationTokenParameter
+            ? await DbContextFactory().Database.BeginTransactionAsync(CancellationToken.None)
+            : await DbContextFactory().Database.BeginTransactionAsync();
+        
+        transaction.Should().NotBeNull();
+    }
+}
